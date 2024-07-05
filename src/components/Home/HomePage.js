5904a3432746c7057485830f9fcf60b8689e3dd4
VanToan <nguyenvantoanhii2k3@gmail.com>
2024-07-05 21:53:11 +0700

    5/7/2024

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,4 +1,23 @@
+import videoHomePage from "../../assert/video-homepage.mp4";
 const HomePage = (props) => {
-  return <div className="homepage-component">home page component</div>;
+  return (
+    <div className="homepage-component ">
+      <video autoPlay muted loop>
+        <source src={videoHomePage} type="video/mp4" />
+      </video>
+      <div className="homepage-content">
+        <div className="homepage-content_text1">
+          Make forms worth filling out
+        </div>
+        <div className="homepage-content_text2">
+          Get more data—like signups, feedback, and anything <br />
+          else—with forms designed to be refreshingly different.
+        </div>
+        <div className="homepage-content_text3">
+          <button className="btn btn-dark">Get started—it's free</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default HomePage;
